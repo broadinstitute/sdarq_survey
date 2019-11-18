746fcd1e7b883195914c50e3b8ac2d58f9c0ae79
Zybjana Bedo <zbedo@broadinstitute.org>
2019-11-18 18:53:37 -0500

    Create Jira ticket

--- backend/app.py
+++ backend/app.py
@@ -4,6 +4,7 @@ from flask_api import FlaskAPI
 from flask_cors import CORS, cross_origin
 import defectdojo as wrapper
 from slacker import Slacker
+from jira import JIRA
 
 # Read config
 with open('config.json') as config_file:
@@ -13,13 +14,21 @@ host = data['host']
 user = data['user']
 api_key = data['api_key']
 slack_token = data['slack_token']
+jira_username = data['jira_username']
+jira_api_token = data['jira_api_token']
+jira_instance = data['jira_instance']
 
+# Slack communication via slack_token
 slack = Slacker(slack_token)
 
 # Instantiate the DefectDojo backend wrapper
 dd = wrapper.DefectDojoAPI(host, api_key, user, debug=False)
 app = FlaskAPI(__name__)
 CORS(app, resources=r'/backend/*')
+
+# Instantiate the Jira backend wrapper
+global jira
+jira = JIRA(basic_auth=(jira_username, jira_api_token),options={'server': jira_instance })
 
 # Prepare to send to DefectDojo
 @app.route('/submit/', methods=['POST', 'GET'])
@@ -33,7 +42,6 @@ def submit():
     product = dd.create_product(appName, "Created for testing", prod_type)
 
     if product.success:
-        # Get the product id
         product_id = product.id()
 
     # Set product
@@ -43,18 +51,31 @@ def submit():
     data3 = data2.strip(']').replace(']',' ')
     data4 = data3.strip('""').replace('"', ' ')
 
+
+    # Create product in DefectDojo
     productDescription = dd.set_product(product_id, description=data4)
 
-    slack.chat.post_message('#dsp-security',
-                            'Project: ' + appName + 'is sent to DefectDojo :notebook: ! The link is here'
-                            ' https://defect-dojo.dsp-techops.broadinstitute.org/product/' +  str(product_id) + '.')
+
+    # Create a Jira ticket
+    if 'JiraProject' in jsonData:
+        project_key_id = jsonData['JiraProject']
+        jira_ticket = jira.create_issue(project=project_key_id,
+                                        summary='New security requirements issue',
+                                        description='Description of new security requirements issue.',
+                                        issuetype={'name': 'Task'})
+        slack.chat.post_message('#dsp-security',
+                                'New service engagement created :notebook_with_decorative_cover: \n 1.Project name: ' + appName + '\n 2.DefectDojo URL: https://defect-dojo.dsp-techops.broadinstitute.org/product/' + str(
+                                  product_id) + '\n 3.Jira Issue Url: https://broadworkbench.atlassian.net/projects/'+ str(project_key_id) + " ")
+    else:
+        slack.chat.post_message('#dsp-security',
+                                'New service engagement created :notebook_with_decorative_cover: \n 1.Project name: ' + appName + '\n 2.DefectDojo URL: https://defect-dojo.dsp-techops.broadinstitute.org/product/' + str(
+                                  product_id) + " ")
 
     response = make_response((json.dumps(data),200,
                        {"X-Frame-Options": "SAMEORIGIN",
                         "X-XSS-Protection":"1; mode=block",
                         "X-Content-type-Options":"nosniff"}))
 
-    # response.headers.set('Content-Security-Policy', "default-src 'self'")
 
     return response
 
