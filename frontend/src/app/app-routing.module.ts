85a274ae53d75ac275efb7eb80138b9e2ed8be10
Zybjana Bedo <zbedo@broadinstitute.org>
2020-01-21 11:28:46 -0500

    Fix routing names

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -6,8 +6,8 @@ import {NonDSPformComponent} from './non-dspform/non-dspform.component';
 import { HomepageComponent } from './homepage/homepage.component';
 
 const routes: Routes = [
-  {path:'', component: HomepageComponent},
-  { path: 'newServicePage', component: MainpageComponent },
+  { path:'', component: HomepageComponent},
+  { path: 'newService', component: MainpageComponent},
   { path: 'questionnaire', component: FormComponent},
   { path: 'notDSPquestionnaire', component: NonDSPformComponent}
 ];
